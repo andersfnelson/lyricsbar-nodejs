ee54ae4e284bf77da747d14e34ff6e26cf7009f1
davidbrekke <davidmagnuson@icloud.com>
2020-03-19 17:59:51 +0000

    update for new db

--- routes/delete.js
+++ routes/delete.js
@@ -2,14 +2,14 @@ module.exports = function (app) {
     app.get('/delete/:id', isLoggedIn, (req, res) => {
         // id from get params
         let songID = req.params.id;
-        let query = `delete from song where id = ${songID}`;
+        let query = `delete from song_table where song_id = ${songID}`;
         db.query(query, (err, result) => {
             if (err) {
                 return res.status(500).send(err);
             }
             res.redirect('/');
-        })
-    })
+        });
+    });
 }
 
 // route middleware to make sure
