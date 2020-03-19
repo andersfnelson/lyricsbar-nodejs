7b376dcb875272eb98dbb3e23b79d75931eae65e
davidbrekke <davidmagnuson@icloud.com>
2020-03-19 18:01:14 +0000

    update for new db

--- routes/update.js
+++ routes/update.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     app.get('/update/:id', isLoggedIn, (req, res) => {
         // get id from get params
         let songID = req.params.id;
-        let query = `select * from song where id = ${songID}`;
+        let query = `select * from song_table where song_id = ${songID}`;
         db.query(query, (err, result) => {
             if (err) {
                 return res.status(500).send(err);
@@ -21,19 +21,18 @@ module.exports = function (app) {
             let genre = req.body.genre
             let year = req.body.year
             let lyrics = req.body.lyrics
-            let songID = req.params.id;
+            let song_id = req.params.id;
             // query for updating song
-            let query = `update song set title = '${title}', artist = '${artist}', album = '${album}', genre = '${genre}', year = '${year}', lyrics = '${lyrics}' where id = ${songID}`;
+            let query = `update song_table set title = '${title}', artist = '${artist}', album = '${album}', genre = '${genre}', year = '${year}', lyrics = '${lyrics}' where song_id = ${song_id}`;
             db.query(query, (err, result) => {
-                if (err) {
-                    return res.status(500).send(err);
-                }
+                if (err) { return res.status(500).send(err); }
                 // query for selecting the song again after being updates
-                let redirectquery = `select * from song where id = ${songID}`;
+                let redirectquery = `select * from song_table where song_id = ${song_id}`;
                 db.query(redirectquery, (err2, result2) => {
                     if (err2) {
                         return res.status(500).send(err2);
                     }
+                    console.log(result2[0].song_id);
                     res.render('displaySong.ejs', {
                         song: result2[0],
                         user: req.user
