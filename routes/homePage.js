cbcfc17c8ff5555f56fce1c3bfe83021a60d5940
davidbrekke <davidmagnuson@icloud.com>
2020-03-19 18:00:26 +0000

    update for new db

--- routes/homePage.js
+++ routes/homePage.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.get('/', (req, res) => {
-        let query = "select id, title, artist, album, genre, year from song"; // query database to get all the players
+        let query = "select song_id, title, artist, album, genre, year from song_table"; // query database to get all the players
         // execute query
         db.query(query, (err, result) => {
             if (err) {
@@ -16,7 +16,7 @@ module.exports = function (app) {
         app.post('/', (req, res) => {
             // getting the search from the req
             let search = req.body.search
-            let query = `select * from song where title or artist or album or genre or year or lyrics like '%${search}%'`;
+            let query = `select * from song_table where title or artist or album or genre or year or lyrics like '${search}'`;
             db.query(query, (err, result) => {
                 if (err) {
                     return res.status(500).send(err);
